96e1f1d789b0c92851267ad9c902c19d6d5c5959
collinskoech11 <collinskoechck34@gmail.com>
2021-11-23 19:38:15 +0300

    typing text animation

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const Article = require('./../models/article')
 const router = express.Router()
+const app = express()
 //route declarations 
+router.use(express.static(__dirname + '../../static'))
 router.get('/new', (req, res) => {
     res.render('articles/new', { article: new Article() })
 })
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost/blog', {
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '../../static'))
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
